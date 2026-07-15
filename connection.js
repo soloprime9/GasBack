0e96a24ec174852bf77a7b7f53685e19be37b5b7
soloprime9 <primeindia5750@gmail.com>
2026-07-16 00:38:53 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 //   mongodb+srv://pratikkumar5750:7iTqxNbaADqvGMtH@cluster0.x12xsyl.mongodb.net/?appName=Cluster0
 // mongodb+srv://pratikkumar5750:mheekd9tSQqWF3ui@cluster0.x12xsyl.mongodb.net/?appName=Cluster0
-mongoose.connect("mongodb+srv://pratikkumar5750:7iTqxNbaADqvGMtH@cluster0.x12xsyl.mongodb.net/?appName=Cluster0")
+mongoose.connect("mongodb+srv://Fondpeace:8Tr4GGxbcusHNUr0@cluster0.x12xsyl.mongodb.net/fondpeace?appName=Cluster0
+")
     .then((result) => 
         console.log("Connected SuccessFully ")
     )
